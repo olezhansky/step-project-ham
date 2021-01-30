b535d16a1d6dbe9500f66d372cf6f576160bbd54
Oleh Ivanov <fenix12895@gmail.com>
2021-01-30 19:00:52 +0200

    modified

--- js/slider.js
+++ js/slider.js
@@ -1,26 +1,58 @@
-var galleryThumbs = new Swiper('.gallery-thumbs', {
-    spaceBetween: 1,
+// var galleryThumbs = new Swiper('.gallery-thumbs', {
+//     spaceBetween: 1,
+//     slidesPerView: 4,
+//     loop: true,
+//     freeMode: true,
+//     loopedSlides: 4, //looped slides should be the same
+//     watchSlidesProgress: true,
+//     navigation: {
+//       nextEl: '.button-next',
+//       prevEl: '.button-prev',
+//     },
+//   });
+//   var galleryTop = new Swiper('.gallery-top', {
+//     spaceBetween: 1,
+//     // slidesPerView: 1,
+//     loop: true,
+//     loopedSlides: 1, //looped slides should be the same
+//     navigation: {
+//       nextEl: '.button-next',
+//       prevEl: '.button-prev',
+//     },
+//     // thumbs: {
+//     //   swiper: galleryTop,
+//     //   // swiper: galleryThumbs
+//     // },
+//     thumbs: {
+//       // swiper: galleryTop,
+//       swiper: galleryThumbs
+//     }
+//   });
+
+  var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
     slidesPerView: 4,
     loop: true,
     freeMode: true,
-    loopedSlides: 4, //looped slides should be the same
+    loopedSlides: 5, //looped slides should be the same
+    watchSlidesVisibility: true,
     watchSlidesProgress: true,
     navigation: {
       nextEl: '.button-next',
-      prevEl: '.button-prev',
-    },
+      prevEl: '.button-prev'
+    }
   });
   var galleryTop = new Swiper('.gallery-top', {
-    spaceBetween: 1,
-    slidesPerView: 1,
+    spaceBetween: 10,
     loop: true,
-    loopedSlides: 1, //looped slides should be the same
+    loopedSlides: 5, //looped slides should be the same
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
     navigation: {
-      nextEl: '.button-next',
-      prevEl: '.button-prev',
-    },
-    thumbs: {
-      swiper: galleryTop
-    },
+        nextEl: '.button-next',
+        prevEl: '.button-prev'
+      }
   });
   
--- js/worksFilter.js
+++ js/worksFilter.js
@@ -1,13 +1,11 @@
 (function() {
     const worksItems = document.querySelectorAll('.works__item');
-    const worksMenuEl = document.querySelector('.works__menu');
-    const worksMenuItemEl = document.querySelectorAll('.works__menu-item ');
-    const worksItemsDdropEl = document.querySelector('.works__items-drop');
-    const worksBtnDropMenuEl = document.querySelector('.works__btn-drop-menu');
-    const worksItemsBlockEl = document.querySelector('.works__items');
-    const worksItemsDropBlockEl = document.querySelector('.works__items-drop');
-    const loadingWorksEl = document.querySelector('.works__cssload-loader');
-    
+    const worksMenu = document.querySelector('.works__menu');
+    const worksMenuItem = document.querySelectorAll('.works__menu-item');
+    const worksItemsBlock = document.querySelector('.works__items');
+    const worksItemsDropBlock1 = document.querySelector('#works__items-drop-1');
+    const worksItemsDropBlock2 = document.querySelector('#works__items-drop-2');
+
     const filterGallery  = (event) => {
         if (event.target.tagName !== 'LI') return false;
         const filterClass = event.target.dataset['filter'];
@@ -16,36 +14,27 @@
             if (!item.classList.contains(filterClass) && filterClass !== 'all-items') {
                 item.classList.add('works__item-hide');
             }
-            worksItemsBlockEl.style.marginLeft = '152px';
-            worksItemsDropBlockEl.style.marginLeft = '152px';
+            worksItemsBlock.style.marginLeft = '152px';
+            worksItemsDropBlock1.style.marginLeft = '152px';
+            worksItemsDropBlock2.style.marginLeft = '152px'
             if (filterClass === 'all-items') {
-                worksItemsBlockEl.style.marginLeft = '0px';
-                worksItemsDropBlockEl.style.marginLeft = '0px'; 
+                worksItemsBlock.style.marginLeft = '0px';
+                worksItemsDropBlock1.style.marginLeft = '0px'; 
+                worksItemsDropBlock2.style.marginLeft = '0px'
             }
         })
     };
     const changeBorderColorInWorksMenuItem = (event) => {
         if (event.target.tagName !== 'LI') return false;
-        for (let menuItem of worksMenuItemEl) {
+        for (let menuItem of worksMenuItem) {
             menuItem.classList.remove('works__menu-item--active')
         }
         event.target.classList.add('works__menu-item--active');
     };
-    const hideLoadingShowDropGallery  = () => { 
-        setTimeout(() => {
-            loadingWorksEl.style.display = 'none';
-            worksItemsDdropEl.style.display = 'grid';
-        }, 2000)
-    };
-    
-    worksMenuEl.addEventListener('click', filterGallery);
-    worksMenuEl.addEventListener('click', changeBorderColorInWorksMenuItem);
-    worksBtnDropMenuEl.addEventListener('click', function () {
-        this.style.display = 'none';
-        loadingWorksEl.style.display = 'block';
-    });
-    worksBtnDropMenuEl.addEventListener('click', hideLoadingShowDropGallery);
+    worksMenu.addEventListener('click', filterGallery);
+    worksMenu.addEventListener('click', changeBorderColorInWorksMenuItem);
 }());
+
 
 
 
